559628463bb32a627b2ad68abf5ab520eecde633
Ryan Medina <ryan.medina@csaa.com>
2020-05-24 22:13:09 -0700

    expose websocket to access close event and end chat session

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -4812,7 +4812,8 @@
                 botDetails: botDetails,
                 chatHistory: chatHistory,
                 addWidgetEvents:addWidgetEvents,
-                setWidgetInstance:setWidgetInstance
+                setWidgetInstance:setWidgetInstance,
+                bot: bot
             };
 
             //Actual chatwindow.js koreBotChat function code end here
