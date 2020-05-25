d6187a0f6011e8eb961632dad355b442373465ac
Ryan Medina <ryan.medina@csaa.com>
2020-05-24 21:15:10 -0700

    create csaaKoreBotChat wrapper around koreBotChat

--- UI/gruntFile.js
+++ UI/gruntFile.js
@@ -90,6 +90,7 @@ module.exports = function (grunt) {
             '../libs/client_api.js',
             // 'kore-config.js',
             // 'kore-main.js',
+            'custom/csaaChatWindow.js',
             'libs/kore-no-conflict-end.js'
         ];
         cssFiles=[
